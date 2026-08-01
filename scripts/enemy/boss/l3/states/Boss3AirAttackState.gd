341c3980ae59c2f34158726a6d9465fc1d2257f8
bangok <2764300331qq.com>
2026-08-01 21:45:46 +0800

    制作4-1

--- scripts/enemy/boss/l3/states/Boss3AirAttackState.gd
+++ scripts/enemy/boss/l3/states/Boss3AirAttackState.gd
@@ -6,7 +6,7 @@ func enter(_msg: Dictionary = {}) -> void:
 	super()
 	boss.velocity.y = 0.0
 	boss.animated_sprite.play("air_attack")
-	AudioManager.play_sound(&"gongji")
+	# 空中攻击使用 SwordHitBox
 	boss.sword_hit_box.set_deferred("monitoring", true)
 
 func update(_delta: float) -> void:
@@ -14,8 +14,11 @@ func update(_delta: float) -> void:
 		boss.sword_hit_box.set_deferred("monitoring", false)
 		state_machine.change_state_by_name("Boss3FallState")
 
-func physics_update(_delta: float) -> void:
+func physics_update(delta: float) -> void:
 	boss.velocity.x = 0.0
+	if not boss.is_on_floor():
+		boss.velocity.y += boss.data.gravity * delta
+	boss.move_and_slide()
 
 func exit() -> void:
 	boss.sword_hit_box.set_deferred("monitoring", false)
